8ed2ebc80b1e0d001c51b44e1d770252c8eed984
liweicheng00 <47774646+liweicheng00@users.noreply.github.com>
2022-12-06 14:51:21 +0800

    update config.py

--- app/config.py
+++ app/config.py
@@ -7,45 +7,22 @@ class Settings(BaseSettings):
     runtime_env: str = os.getenv('RUNTIME_ENV')
     docs_url: Optional[str] = None
 
-    mongodb_db: str = os.getenv('MONGODB_DB')
-    mongodb_host: str = os.getenv('MONGODB_HOST')
-    mongodb_user: str = os.getenv('MONGODB_USER')
-    mongodb_password: str = os.getenv('MONGODB_PASSWORD')
-    mongodb_ca_cert: str = os.getenv('MONGODB_CA_CERT')
-
-    access_token_secret_key: str = os.getenv("ACCESS_TOKEN_SECRET_KEY")
-    access_token_expire_minutes: int = 60
-    audience: str = "http://localhost"
-    issuer: str = "http://localhost"
-
-    frontend_domain: HttpUrl = "https://o2meta.io"
-
-    slack_notification_webhook: str = ""
-
-    o2_redis_host: str = os.getenv('O2_REDIS_HOST')
-    o2_redis_port: Union[int, str] = os.getenv('O2_REDIS_PORT') or ""
-
-    db_index: int = 0
+    project_name: str = "sp-custom-domain"
+    s3_bucket_domain_name: str = os.getenv('S3_BUCKET_DOMAIN_NAME')
+    default_project_domain_name: str = os.getenv('DEFAULT_PROJECT_DOMAIN_NAME')
+    hosted_zone_id: str = os.getenv('HOSTED_ZONE_ID')
 
 
 class StagingSettings(Settings):
     docs_url: Optional[str] = '/docs'
-    trusted_hosts: list = None
-    origins: list = []
-
-    contract_address: str = ""
-    web3_url: str = "https://eth-rinkeby.alchemyapi.io/v2/"
-    alchemy_token_key: str = ""
-    graph_url: str = ""
 
 
 class DemoSettings(StagingSettings):
-    db_index: int = 3
     ...
 
 
 class HotfixStagings(StagingSettings):
-    db_index: int = 6
+    ...
 
 
 class TestSettings(StagingSettings):
@@ -58,14 +35,10 @@ class TestSettings(StagingSettings):
 class ProdSettings(Settings):
     origins: list = []
     trusted_hosts: list = []
-    contract_address: str = ""
-    web3_url: str = "https://eth-mainnet.alchemyapi.io/v2/"
-    alchemy_token_key: str = ""
-    graph_url: str = ""
 
 
 class RcSettings(ProdSettings):
-    db_index: int = 3
+    ...
 
 
 RUNTIME_ENV = os.getenv('RUNTIME_ENV')
